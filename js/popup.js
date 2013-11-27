63ff6fb828708074a323e1520926c09b37d4f1aa
David <ariedov@gmail.com>
2013-11-27 14:30:51 +0200

    made relative commit count color chooser

--- js/popup.js
+++ js/popup.js
@@ -4,10 +4,10 @@ var githubProgressGenerator = {
 	
 	colors: [
 		"#eeeeee", // less than 1 commits
-		"#d6e685", // less than 3 commits 
-		"#8cc665", // less than 5 commits
-		"#44a340", // less than 7 commits
-		"#1e6823", // less than 9 commits
+		"#d6e685",
+		"#8cc665", 
+		"#44a340",
+		"#1e6823",
 	],
 		
     requestProgress: function() {
@@ -28,20 +28,24 @@ var githubProgressGenerator = {
 		var data = currentProgress[0];
 		var commitsCount = currentProgress[1];
 		
-		icon.style.visibility = 'visible';
-		icon.style.background = this.chooseColor_(commitsCount);
+		var maxCommits = this.getMaxCommitCount_(yearProgress);
+		icon.style.visibility = 'visible';		
+		icon.style.background = this.chooseColor_(maxCommits, commitsCount);
 		commits.innerText = commitsCount;
 	},	
 	
-	chooseColor_: function(commits) {
+	chooseColor_: function(maxCommits, commits) {
 		var colors = this.colors;
+		var delta = Math.ceil(maxCommits / 4);
+		console.log("Max: " + maxCommits + " delta: " + delta);		
+		
 		if (commits < 1) {
 			return colors[0];
-		} else if (commits <= 3) {
+		} else if (commits <= maxCommits - 3 * delta) {
 			return colors[1];
-		} else if (commits <= 5) {
+		} else if (commits <= maxCommits - 2 * delta) {
 			return colors[2];
-		} else if (commits <= 7) {
+		} else if (commits <= maxCommits - delta) {
 			return colors[3];
 		} else {
 			return colors[4];
@@ -66,6 +70,18 @@ var githubProgressGenerator = {
 		}
 		return savedAccount;
 	},
+	
+	getMaxCommitCount_: function(commitHistory) {
+		var maxCommitCount = commitHistory[0][1];
+		var observerableCommits = commitHistory.length - 1;
+		for (var i = observerableCommits; i > observerableCommits - 14; i--) {
+			if (maxCommitCount < commitHistory[i][1]) {
+				maxCommitCount = commitHistory[i][1];
+			}
+		}
+		
+		return maxCommitCount;
+	}
 }
 
 document.addEventListener('DOMContentLoaded', function () {
